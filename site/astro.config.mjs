14ad92cb4023bbb7705206f4b4d4e2f5deea47f6
Anish Mangal <anishmg@umich.edu>
2025-11-06 16:08:54 +0530

    Update buggy g-h-pages workflow and astroconfig

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+  // 👇 When using GitHub Pages, Astro needs the repo subpath.
+  // When you move to starstucklab.com, you can remove the `base` line entirely.
   site: 'https://starstucklab.com',
+  base: process.env.DEPLOY_ENV === 'ghpages' ? '/starstucklab/' : '/',
   integrations: [],
   vite: {
     server: {
@@ -11,4 +14,3 @@ export default defineConfig({
     }
   }
 });
-
