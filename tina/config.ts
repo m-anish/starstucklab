fd47d1a12941fdc8ec96ac02d188b7700d9f8cf5
Anish Mangal <anishmg@umich.edu>
2025-12-11 18:19:58 +0530

    fix: update mediaRoot path in TinaCMS configuration

--- tina/config.ts
+++ tina/config.ts
@@ -21,8 +21,8 @@ export default defineConfig({
   },
   media: {
     tina: {
-      mediaRoot: "",
       publicFolder: "public",
+      mediaRoot: "assets",
     },
   },
   // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/r/content-modelling-collections/
